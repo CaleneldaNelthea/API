a76a2eee6bc96b291622b765197659af74867347
Arasjie <martijn.valkenborgh@student.pxl.be>
2021-11-17 19:20:19 +0100

    finishing signup

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-
+const https = require("https");
 
 const app = express();
 //static is needed because bootstrap is loaded remote and css is local
@@ -13,13 +13,54 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function(req, res) {
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
-  console.log(firstName, lastName, email);
-})
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us20.api.mailchimp.com/3.0/lists/23c4e689eb";
+  const options = {
+    method: "POST",
+    auth: "martijn1:0bba5d40349ae15d80c511546e01f0e1-us20"
+  }
+
+  const request = https.request(url, options, function(response) {
+    if (response.statusCde === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } res.sendFile(__dirname + "/failure.html");
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
 });
+
+// API key
+// 0bba5d40349ae15d80c511546e01f0e1-us20
+
+// List ID
+// 23c4e689eb
